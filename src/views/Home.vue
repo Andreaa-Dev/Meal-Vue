1827b871e34771b1b9f76036413013bc5852faa0
andrea <ngocanhhsgs@gmail.com>
2023-06-13 14:25:39 +0300

    working on form

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,20 @@
-<template>Home</template>
+<template>
+  <div>
+    <input
+      type="text"
+      class="rounded border-2 border-gray-200"
+      placeholder="Search meal by name"
+    />
+    <div>
+      <router-link to="/" v-for="letter of letters">{{ letter }}</router-link>
+    </div>
+  </div>
+</template>
 
-<script setup></script>
+<script setup>
+import { computed } from "vue";
+import store from "../store";
+
+const meals = computed(() => store.state.meals);
+const letters = "ABCDEFJHIJKLMNOPQRSTUWXYZ".split("");
+</script>
